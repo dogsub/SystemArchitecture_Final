ac7330037d158a74f06b40943c9ad79363512232
dogsub <ehdtjq0909@gmail.com>
2024-11-24 05:21:54 +0900

    Test CI workflow5

--- DREANK_BACK-back/src/main/java/gcu/backend/dreank/domain/calendar/TimeSlot.java
+++ DREANK_BACK-back/src/main/java/gcu/backend/dreank/domain/calendar/TimeSlot.java
@@ -10,7 +10,7 @@ import java.time.LocalTime;
 @Setter
 @AllArgsConstructor
 public class TimeSlot {
-    // test3
+    // test5
     private LocalTime startTime;
     private LocalTime endTime;
     private Day day;
